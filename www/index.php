d37c13f77563b5c58bf28cb309f310aaa8051948
76zzz29 <76zzz29@gmail.com>
2017-06-26 21:31:57 +0200

    Update index.php

--- www/index.php
+++ www/index.php
@@ -2,7 +2,7 @@
 //	fait par 76zzz29 20/20 en programation 5/20 en style, 2/20 en français.
 readfile($_SERVER["DOCUMENT_ROOT"] . "/../data/base/1-3");
 readfile($_SERVER["DOCUMENT_ROOT"] . "/../data/base/2-3");
-echo "<h1><img src="images/title.jpg" alt="[?][titre]" height="[?]" width="[?]" /></h1>";
+echo "<h1><img src="/images/title.jpg" alt="[?][titre]" height="[?]" width="[?]" /></h1>";
 echo "<ul>
   <li>connection</li>
   <li><créer un compte</li>
