fc9034395f9c3f4dcb2d7c9b3a84a71c558f9554
76zzz29 <76zzz29@gmail.com>
2017-06-26 21:03:49 +0200

    Create index.php

---
+++ www/index.php
@@ -0,0 +1,13 @@
+<?php
+//	fait par 76zzz29 20/20 en programation 5/20 en style, 2/20 en français.
+readfile($_SERVER["DOCUMENT_ROOT"] . "/../data/base/1-3");
+readfile($_SERVER["DOCUMENT_ROOT"] . "/../data/base/2-3");
+echo "<h1><img src="images/title.jpg" alt="[?][titre]" height="[?]" width="[?]" /></h1>";
+<ul>
+  <li>connection</li>
+  <li><créer un compte</li>
+  <li>un problem/un bug</li>
+  <li>faq</li>
+  <li>about</li>
+include($_SERVER["DOCUMENT_ROOT"] . "/../data/base/3-3");
+?>
