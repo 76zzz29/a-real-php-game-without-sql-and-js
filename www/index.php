ad9bb454d84ece3bbca81c3c5f3097a2fed7da6f
76zzz29 <76zzz29@gmail.com>
2017-06-27 12:03:40 +0200

    Add files via upload

--- www/index.php
+++ www/index.php
@@ -1,13 +1,12 @@
 <?php
 //	fait par 76zzz29 20/20 en programation 5/20 en style, 2/20 en français.
-readfile($_SERVER["DOCUMENT_ROOT"] . "/../data/base/1-3");
-readfile($_SERVER["DOCUMENT_ROOT"] . "/../data/base/2-3");
-echo "<h1><img src="/images/title.jpg" alt="[?][titre]" height="[?]" width="[?]" /></h1>";
-echo "<ul>
+include $_SERVER["DOCUMENT_ROOT"] . "/../data/base/1-3" ;
+include $_SERVER["DOCUMENT_ROOT"] . "/../data/base/2-3" ;
+echo '<ul>
   <li>connection</li>
-  <li><créer un compte</li>
+  <li>créer un compte</li>
   <li>un problem/un bug</li>
   <li>faq</li>
-  <li>about</li>";
-readfile($_SERVER["DOCUMENT_ROOT"] . "/../data/base/3-3");
-?>
+  <li>about</li>
+</ul>';
+include $_SERVER["DOCUMENT_ROOT"] . "/../data/base/3-3" ;?>
