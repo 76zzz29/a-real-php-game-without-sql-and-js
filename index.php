0c8e31dd4f0e0fcb77047ce6c9b708355bd1b6e1
76zzz29 <76zzz29@gmail.com>
2017-06-27 12:18:35 +0200

    Update index.php

--- index.php
+++ index.php
@@ -4,13 +4,13 @@
 //	vous pourez voire que c'est un jeux a héberger a la racine du siteavec des fichier se trouvant dans un docier 'data'
 //	qui se situe au meme niveau que la racine du site donc il ne poura pas etre héberger sur un hébergeur 'clasic'
 //	en  incluants uniquement 1-3, 2-3 et 3-3 normalement on a une page de base vide sans contenus et sans erreur avec la mise en page 
-
-//	include($_SERVER["DOCUMENT_ROOT"] . "/../data/base/1-3");	//	tout le début de l'html est la dedan mais la balise
+//
+//	include $_SERVER["DOCUMENT_ROOT"] . "/../data/base/1-3" ;	//	tout le début de l'html est la dedan mais la balise
 //																														//	head reste ouverte
 //
 //	echo" <link href="/css/css.css" rel="stylesheet" type="text/css"> ";
 //
-//	include($_SERVER["DOCUMENT_ROOT"] . "/../data/base/2-3");	//	fermeture de la balise head , ouverture de la balise body
+//	include $_SERVER["DOCUMENT_ROOT"] . "/../data/base/2-3" ;	//	fermeture de la balise head , ouverture de la balise body
 //																														//	et début comun a toutes les pages
 //
 //	------------------[ICI VIENDRA TOUT LE BORDEL DE LA PAGE]------------------
@@ -26,6 +26,7 @@
 //
 //
 //
-//	include($_SERVER["DOCUMENT_ROOT"] . "/../data/base/3-3");	// fin du fichier contenant le pied de page et la fin du html
+//
+//	include $_SERVER["DOCUMENT_ROOT"] . "/../data/base/3-3" ;	// fin du fichier contenant le pied de page et la fin du html
 //
 ?>
