e3d5ed7e7b7ba41f457cd060752eccf339c83297
76zzz29 <76zzz29@gmail.com>
2017-09-03 16:47:00 +0200

    Add files via upload

--- www/login/index.php
+++ www/login/index.php
@@ -3,11 +3,6 @@ include_once $_SERVER["DOCUMENT_ROOT"] . "/../data/base/variable" ;
 //	fait par 76zzz29 20/20 en programation 5/20 en style, 2/20 en français.
 include_once $_SERVER["DOCUMENT_ROOT"] . "/../data/base/1-3" ;
 include_once $_SERVER["DOCUMENT_ROOT"] . "/../data/base/2-3" ;
-echo '<ul>
-  <li><a href="/login/" title="login">connection</a></li>
-  <li>cr&eacute;er un compte</li>
-  <li>un problem/un bug</li>
-  <li>faq</li>
-  <li>about</li>
-</ul>';
+
+
 include_once $_SERVER["DOCUMENT_ROOT"] . "/../data/base/3-3" ;?>
