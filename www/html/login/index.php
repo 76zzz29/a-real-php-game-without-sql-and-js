0c89b9fef34db502564242abb441e2bcfdd679b8
76zzz29 <76zzz29@gmail.com>
2017-09-03 16:41:37 +0200

    Delete index.php

--- www/html/login/index.php
+++
@@ -1,8 +0,0 @@
-<?php
-include_once $_SERVER["DOCUMENT_ROOT"] . "/../data/base/variable" ;
-//	fait par 76zzz29 20/20 en programation 5/20 en style, 2/20 en français.
-include_once $_SERVER["DOCUMENT_ROOT"] . "/../data/base/1-3" ;
-include_once $_SERVER["DOCUMENT_ROOT"] . "/../data/base/2-3" ;
-
-
-include_once $_SERVER["DOCUMENT_ROOT"] . "/../data/base/3-3" ;?>
